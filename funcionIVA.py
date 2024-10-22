a92ea0d590e89118d4ad574b3e752da35dc80289
Chad154 <chad154154@gmail.com>
2024-10-22 11:43:43 +0200

    funcionIVA

--- funcionIVA.py
+++ funcionIVA.py
@@ -1,5 +1,5 @@
-def calcularIVA():
-    importe = int(input("Precio del producto: "))
+def calcularIVA(importe):
+    importe = int(input(f"Precio del producto: {importe}"))
     total = importe * 1.21
     print(f"IVA incluido (21%): {total}")
     return
